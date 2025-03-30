7f8efa5f2a339e5ba6424793eec93f7152e6cbc2
VineethKatta-07 <vineethkatta321@gmail.com>
2025-03-30 21:44:35 +0530

    Add more files

---
+++ functions/index.js
@@ -0,0 +1,11 @@
+
+// this is a demo function that prints hello world to the console.
+function demoFunction() {
+    console.log("Hello world!")
+}
+
+const demoFun2 = ()=>{
+    return "Hello world!"
+} 
+
+console.log(demoFun2())
